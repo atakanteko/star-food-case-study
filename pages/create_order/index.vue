be85b3b8013828f467d8d67be68d45d09e671a13
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-11 13:24:04 +0300

    [atakan] add create order page

---
+++ pages/create_order/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <HeaderComponent title="Create Order" />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '../../components/Header';
+
+export default {
+  name: 'CreateOrderComponent',
+  components: { HeaderComponent },
+};
+</script>
+<style scoped></style>
