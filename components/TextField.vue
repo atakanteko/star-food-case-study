4e36b28ec7bbc433b9839854b53779ba8cfecd69
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-11 22:34:02 +0300

    [atakan] create input box component

---
+++ components/TextField.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <span>{{ textAreaLabel }}</span>
+    <v-row>
+      <v-col cols="12" sm="12"
+      >
+        <v-text-field
+          v-model="inputValue"
+          class="text-field-input"
+          solo
+          flat
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TextField',
+  props: {
+    textAreaLabel: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      inputValue: null,
+    };
+  },
+};
+</script>
+<style scoped>
+span {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 21px;
+  letter-spacing: 0.05em;
+  color: #171719;
+}
+.text-field-input{
+  background: #FFFFFF;
+  border: 1.5px solid #CCCCCC;
+  box-sizing: border-box;
+  border-radius: 5px;
+  height: 50px;
+  width: 730px;
+  margin-top: 13px;
+}
+</style>
