9c3a3bceeee4419b5b1b87149f8fc439c6124f2b
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-14 01:49:04 +0300

    [atakan] add test for orderlist/dashboard

---
+++ test/Dashboard.spec.js
@@ -0,0 +1,51 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Vuetify from 'vuetify';
+import Dashboard from '../components/OrderList/Dashboard';
+
+const model =
+  [
+    {
+      name: '0537 295 8282',
+      contact: 'Ata',
+      radio: 'Takeaway',
+      clientMessage: 'Ekstra ketçap ve mayonez istiyorum.',
+      orderedItems: [{
+        id: '224471', name: '3 Chicken Wings', price: '250.00', quantity: 1, cost: 250,
+      }, {
+        id: '224474', name: 'Chicken Livers and Portuguese Roll', price: '250.00', quantity: 1, cost: 250,
+      }],
+      totalCost: 500,
+    },
+  ];
+function mountComponent() {
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
+
+  const getListModelMock = jest.fn();
+  getListModelMock.mockReturnValue(model);
+
+  const store = new Vuex.Store({
+    getters: {
+      'store/getListModel': getListModelMock,
+    },
+
+  });
+
+  return shallowMount(Dashboard, {
+    localVue,
+    store,
+    vuetify: new Vuetify(),
+    stubs: {
+    },
+    mocks: {
+    },
+  });
+}
+
+describe('Order List - Dashboard File', () => {
+  test('Component should mount successfully', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.exists()).toBeTruthy();
+  });
+});
