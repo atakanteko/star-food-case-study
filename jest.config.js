fbd9b81090990462a82832ae1570848a8b0e1f7a
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-03-14 01:05:25 +0300

    [atakan] fix config file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  setupFiles: ['./test/setup.js', 'jest-localstorage-mock'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/fileMock.js',
     '^@/(.*)$': '<rootDir>/$1',
